1b2566c216b351893c139565ace5c2d5e108e5e7
Christina Sarai Atoche Pascual <christinas.apascual@gmail.com>
2024-11-05 15:31:58 -0500

    feat: Actualiza alerta en caso de no encontrar coincidencias

--- js/app.js
+++ js/app.js
@@ -1,24 +1,25 @@
 // SELECTORES
 const form = document.querySelector('#form');
-const ciudad = document.querySelector('#ciudad');
-const pais = document.querySelector('#pais');
+const city = document.querySelector('#ciudad');
+const country = document.querySelector('#pais');
 const result = document.querySelector('#result');
 
 // EVENTOS
-loadEvents();
-function loadEvents() {
-    form.addEventListener('submit', validate);
-}
+window.addEventListener('load', () => {
+    form.addEventListener('submit', validate)
+})
 
 
 // FUNCIONES
 function validate(event) {
     event.preventDefault();
 
-    if(ciudad.value.trim() === '' || pais.value.trim() === '') {
+    if(city.value.trim() === '' || country.value.trim() === '') {
         alert('Error: Ambos campos son obligatorio');
         return;
     }
+
+    consultAPI(city.value, country.value)
 }
 
 
@@ -36,7 +37,23 @@ function alert(msg) {
     
     result.appendChild(alert);
 
+    // duración de alerta: 3s
     setTimeout(() => {
         alert.remove();
     }, 3000)
+}
+
+// consulta API
+function consultAPI(city, country) {
+    const appId = "8d2f6b17aa992ae57cb3a68bd1be6f16";
+
+    const urlAPI = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${appId}`;
+
+    fetch(urlAPI)
+        .then(result => result.json())
+        .then(data => {
+            if(data.cod === '404') {
+                alert('Upps. No hemos encontrado coincidencias');
+            }
+        })
 }
